1b3000ac749ae0709713efcf4c3a1f1a51290f85
Dymendes <dymealves@gmail.com>
2023-06-06 00:31:12 -0300

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,31 @@
 <template>
   <main class="home">
     <h1>Home</h1>
+
+    <section v-for="image in images" :key="image._id">
+      <h1>{{ image.title }}</h1>
+    </section>
   </main>
 </template>
 
 <script>
+  import { ref, onMounted } from "vue"
+
+  import api from "../config/axios.js"
 
   export default {
     setup() {
-      
+      const images = ref([])
+
+      const fetch = () => {
+        api.get("/image").then(response => {
+          console.log(response.data.images); images.value = response.data.images
+        })
+      }
+
+      onMounted(fetch)
+
+      return { images }
     }
   }
 </script>
